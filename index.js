d85072f6dda34c1eb35b543186d6840b1f02ebf0
Daniel Parra Riera <danielparrariera7@gmail.com>
2026-02-24 20:02:09 +0100

    Extension

---
+++ index.js
@@ -0,0 +1,17 @@
+var message = "Hola mundo";
+var conteo = 3;
+console.log(message);
+console.log(conteo);
+var saludar = function (saludo) {
+    return console.log("Hola ".concat(saludo));
+};
+saludar("Alice");
+var newUser = { id: 1, nombre: "Daniel", email: "hdhhs@gmail.com", premium: true }; // Usuario
+/***/
+var name = "Daniel";
+var age = 24;
+var newCharacter = {
+    name: "Jose",
+    age: 23
+};
+console.log(newCharacter);
---
+++ index.ts
@@ -0,0 +1,38 @@
+let message:string = "Hola mundo";
+let conteo:number = 3;
+
+console.log(message)
+console.log(conteo)
+
+const saludar = (saludo:string)=>{
+        return console.log(`Hola ${saludo}`)
+}
+
+saludar("Alice")
+
+interface Usuario {
+    id: number,
+    nombre: string,
+    email: string,
+    premium?: boolean // El ? significa que es opcional
+}
+
+const newUser:Usuario = {id:1,nombre:"Daniel",email:"hdhhs@gmail.com",premium:true} // Usuario
+
+/***/
+
+let name:string = "Daniel"
+let age:number = 24
+
+
+interface character { // Typa el objeto
+    name:string
+    age:number
+}
+
+const newCharacter:character = { // Asigna valores al objeto
+    name: "Jose",
+    age: 23
+}
+
+console.log(newCharacter)
